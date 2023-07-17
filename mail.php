c2663c5f9e7a9a86266e39b3cbdf7e3e2469c24e
AkseleratDarko <120180123+AkseleratDarko@users.noreply.github.com>
2023-07-18 00:05:27 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,13 +12,13 @@ $phone = $_POST['user_phone'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'imaginestudio4166@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'uTaa-ufORK91'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('denis.lineage2@mail.ru');     // Кому будет уходить письмо 
+$mail->setFrom('imaginestudio4166@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('imaginestudio4166@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
